0996763e719d04e061ecca6f0a812742133dc7b0
V Hedayati <badvad@gmail.com>
2022-05-26 19:24:34 +0100

    tidy up of test class

--- src/test/java/com/example/SpringBootPostgres/controller/PersonPostgresControllerTest.java
+++ src/test/java/com/example/SpringBootPostgres/controller/PersonPostgresControllerTest.java
@@ -1,53 +1,28 @@
 package com.example.SpringBootPostgres.controller;
 
 import com.example.SpringBootPostgres.api.PersonPostgresController;
-import com.example.SpringBootPostgres.dao.PostgresDBDaoService;
 import com.example.SpringBootPostgres.model.Person;
 import com.example.SpringBootPostgres.service.PersonPostgresService;
-;
-import org.aspectj.lang.annotation.Before;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
-import org.mockito.InjectMocks;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
 import java.util.List;
 import java.util.UUID;
-
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 @WebMvcTest(PersonPostgresController.class)
 public class PersonPostgresControllerTest {
 
-  /*
-   @InjectMocks
-    private PersonPostgresController personPostgresController;
-
-    @Before("")
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        this.mockMvc =  MockMvcBuilders.standaloneSetup(personPostgresController).build();
-    }
-*/
     @MockBean
     private PersonPostgresService personPostgresService;
-
-    @MockBean
-    private PostgresDBDaoService PostgresDBDaoService;
 
     @Autowired
     private MockMvc mockMvc;
@@ -64,9 +39,6 @@ public class PersonPostgresControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.text").value("person was not added"));
 
     }
-
-
-
 
     @Test
     void shouldCreatePerson() throws Exception {
@@ -100,7 +72,7 @@ public class PersonPostgresControllerTest {
         when(personPostgresService.deletePerson(Mockito.any(UUID.class))).thenReturn(true);
         ResultActions action = mockMvc.perform(MockMvcRequestBuilders.delete("/api/v1/personpostgres/"+UUID.randomUUID()))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                        .andExpect(MockMvcResultMatchers.jsonPath("$.text").value("success user has been removed"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$.text").value("success user has been removed"));
     }
 
 
